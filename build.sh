280aba5059c6dc5d06808b0e05a3332319cf5c2c
Fedor Burdun <fedor.burdun@azulsystems.com>
2015-04-10 17:53:03 +0300

    fix dist target. Distribution should contain ioHiccup.jar and some scripts additionally

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -x
 
-rm -rf target
-mvn assembly:assembly
-cp target/ioHiccup.jar ./
+mvn clean install
 
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 javac ioHiccupTest/src/iohiccuptest/IoHiccupTest.java
-java -javaagent:ioHiccup.jar -Xbootclasspath/a:ioHiccup.jar -cp ioHiccupTest/src/ iohiccuptest.IoHiccupTest -t:1999 -i:1999:500 -i:1999:540 &> test.log
+java -javaagent:target/ioHiccup.jar -Xbootclasspath/a:target/ioHiccup.jar -cp ioHiccupTest/src/ iohiccuptest.IoHiccupTest -t:1999 -i:1999:500 -i:1999:540 &> test.log
 
